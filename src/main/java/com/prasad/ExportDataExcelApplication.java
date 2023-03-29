1139aa9a5047ccc1a47821da6fbf8669c77afa6c
Prasad3898 <a.prasadreddy25@gmail.com>
2023-03-29 22:46:05 +0530

    excelreport

---
+++ src/main/java/com/prasad/ExportDataExcelApplication.java
@@ -0,0 +1,13 @@
+package com.prasad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExportDataExcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExportDataExcelApplication.class, args);
+	}
+
+}
